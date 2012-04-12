c6250d55d0e855f391dfb3bfaca5eb4330b3c8e7
Alexandru Marinescu <almarinescu@gmail.com>
2012-04-12 18:07:16 -0400

    Setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="python-hamt",
+	author="Alexandru Marinescu",
+	author_email="almarinescu@gmail.com",
+	version="0.0.1",
+	url="https://github.com/almarinescu/python-hamt"
+)
